29882a70614792a9e437ffda5781323b8e51db3c
Joshua Jolley <josh@jcjolley.com>
2018-06-10 09:35:25 -0600

    Going back

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { setupServer } from "./server";
-import * as fs from 'fs';
-import * as https from 'https';
 const program = require('commander');
 
 program
@@ -8,13 +6,8 @@ program
   .option('-p, --port <n>', 'Specify port to run on', parseInt)
   .parse(process.argv)
 
-const options = {
-  cert: fs.readFileSync('./sslcert/fullchain.pem'),
-  key: fs.readFileSync('./sslcert/privkey.pem')
-}
 
 const app = setupServer();
 const port = program.port || 3002;
 app.listen(port);
-https.createServer(options, app).listen(4002);
 console.log(`Server started on port ${port}`);
