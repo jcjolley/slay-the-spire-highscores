942d874529d407605205650c0d0c9da053c59249
Joshua Jolley <josh@jcjolley.com>
2018-08-11 21:45:15 -0600

    Trying https, again

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { setupServer } from "./server";
 const program = require('commander');
+import fs = require('fs');
+import https = require('https');
 
 program
   .version('1.0.0')
@@ -7,7 +9,13 @@ program
   .parse(process.argv)
 
 
+const options = {
+  cert: fs.readFileSync('/etc/letsencrypt/live/spire.jcjolley.com/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/spire.jcjolley.com/privkey.pem'),
+}
+
 const app = setupServer();
 const port = program.port || 3002;
 app.listen(port);
+https.createServer(options, app).listen(8443);
 console.log(`Server started on port ${port}`);
--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,8 @@ export function setupServer() {
   setupWss();
   const app = express();
 
+
+
   app.use(express.static('static'))
   app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
